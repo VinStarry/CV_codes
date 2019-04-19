ad6af023cfff074eda25f35217b44bccb59b7c2e
Vinstarry <vinstarry@outlook.com>
2019-04-19 21:05:08 +0800

    init project

---
+++ elementary/knn/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+const std::string training_images = "../mnist/train-images.idx3-ubyte";
+const std::string training_labels = "../mnist/train-labels.idx1-ubyte";
+const std::string test_images = "../mnist/t10k-images.idx3-ubyte";
+const std::string test_labels = "../mnist/t10k-labels.idx1-ubyte";
+
+int main() {
+
+    return 0;
+}
