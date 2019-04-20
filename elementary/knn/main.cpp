a80ccdc8ea7bc3f9653f0b19cdc96e2b9adbcba4
Vinstarry <vinstarry@outlook.com>
2019-04-20 15:27:18 +0800

    bug needs fixed

--- elementary/knn/main.cpp
+++ elementary/knn/main.cpp
@@ -18,32 +18,34 @@ int main() {
     /* Get training image and label sets from training files */
     train_parse.get_train_images_with_label_from_mnist(training_set);
 
-    /* Create a KNN model and train it */
-    Ptr<ml::KNearest> knn_model = ml::KNearest::create();
-    knn_model->setDefaultK(5);  // set parameter k for the KNN
-    knn_model->setIsClassifier(true);
-    knn_model->train(training_set, 0); // train this KNN model with training set
-
     /* Get test image and label sets from test files */
     cv::Mat test_set, test_label_set;
     test_parse.get_test_images_with_label_from_mnist(test_set, test_label_set);
 
-    /* Predict result */
-    cv::Mat result_set;
-    knn_model->predict(test_set, result_set, 0);
+    /* Create a KNN model and train it */
+    Ptr<ml::KNearest> knn_model = ml::KNearest::create();
+    knn_model->train(training_set, 0); // train this KNN model with training set
+    for (int K_value = 50; K_value <= 50; K_value++) {
+        knn_model->setDefaultK(K_value);  // set parameter k for the KNN
+        knn_model->setIsClassifier(true);
 
-    /* Calculate accuracy of the prediction */
-    int cnt{0};
-    for (int index = 0; index < test_parse.get_number_of_images(); index++) {
-        int predict_label = static_cast<int>(result_set.data[index]);
-        int acutual_label = static_cast<int>(test_label_set.data[index]);
+        /* Predict result */
+        cv::Mat result_set;
+        knn_model->predict(test_set, result_set, 0);
 
-        if (predict_label == acutual_label)
-            cnt++;
+        /* Calculate accuracy of the prediction */
+        int cnt{0};
+        for (int index = 0; index < test_parse.get_number_of_images(); index++) {
+            int predict_label = static_cast<int>(result_set.data[index]);
+            int actual_label = static_cast<int>(test_label_set.data[index]);
+
+            if (predict_label == actual_label)
+                cnt++;
+        }
+
+        double accuracy_rate = cnt * 1.0 / test_parse.get_number_of_images();
+
+        cout << "When K is " << knn_model->getDefaultK() << ", Test cases: " << test_parse.get_number_of_images()  << ", Correct cases: " << cnt << ", the accuracy rate is:" << accuracy_rate * 100 << endl;
     }
-
-    double accuracy_rate = cnt * 1.0 / test_parse.get_number_of_images();
-
-    cout << "When K is " << knn_model->getDefaultK() << ", the accuracy rate is:" << accuracy_rate * 100 << endl;
     return 0;
 }
